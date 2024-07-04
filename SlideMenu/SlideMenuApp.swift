fe17f8921d67ba7618bd2a4457504ab6b167d0d3
SardorbekSaydamatov <sardorbek.saydamatov@mail.ru>
2024-07-04 15:09:32 +0500

    Initial Commit

---
+++ SlideMenu/SlideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideMenuApp.swift
+//  SlideMenu
+//
+//  Created by Sardorbek Saydamatov on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SlideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
